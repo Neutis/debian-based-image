bad1d62105c27b8478ed6a20814f8d30429a1069
yashin-alexander <alexandr.yashin@emlid.com>
2018-11-12 19:03:44 +0300

    export-image/02-fetch-boot/00-run.sh: add uInitrd image from meta-emlid-neutis

--- export-image/02-fetch-boot/00-run.sh
+++ export-image/02-fetch-boot/00-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+ROCKO_META_EMLID_NEUTIS_HASH=858be875304f728db22a7f921dfd2ee635b9a191
+
 linux_allwinner_dir="${LINUX_SRC}/arch/arm64/boot/dts/allwinner"
 linux_overlay_dir="${linux_allwinner_dir}/overlay"
 
@@ -37,3 +39,6 @@ cp ${linux_overlay_dir}/sun50i-h5-usbhost2.dtbo $OVERLAY_DIR
 cp ${linux_overlay_dir}/sun50i-h5-usbhost3.dtbo $OVERLAY_DIR
 cp ${linux_overlay_dir}/sun50i-h5-hdmi.dtbo $OVERLAY_DIR
 cp ${linux_overlay_dir}/sun50i-h5-camera-status-okay.dtbo $OVERLAY_DIR
+
+git -C ${META_EMLID_NEUTIS_SRC} checkout ${ROCKO_META_EMLID_NEUTIS_HASH} >-
+cp ${META_EMLID_NEUTIS_SRC}/meta-neutis-bsp/recipes-bsp/neutis-initramfs/files/uInitrd ${BOOT_DIR}
