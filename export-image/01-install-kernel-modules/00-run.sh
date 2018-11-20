99577aff60f08c507a9fb75ec97fba7ed218881f
yashin-alexander <alexandr.yashin@emlid.com>
2018-11-20 15:46:47 +0300

    export-image/01-install-kernel-modules/00-run.sh: use separate functions for .deb install

--- export-image/01-install-kernel-modules/00-run.sh
+++ export-image/01-install-kernel-modules/00-run.sh
@@ -1,19 +1,12 @@
 #!/bin/bash -e
 
 PACKAGE_NAME=neutis-n5-kernel-modules
-KERNEL_MODULES_DEB_DIR=${STAGE_WORK_DIR}/${PACKAGE_NAME}
+KERNEL_MODULES_DEB_DIR=${STAGE_WORK_DIR}/packages/${PACKAGE_NAME}
 
 mkdir -p ${KERNEL_MODULES_DEB_DIR}/lib/modules/
 mkdir -p ${KERNEL_MODULES_DEB_DIR}/DEBIAN
-rm -rf ${ROOTFS_DIR}/var/cache/apt/archives
-mkdir -p ${ROOTFS_DIR}/var/cache/apt/archives
 
 cp -R ${LINUX_SRC}/build-modules/lib/modules/${LINUX_VERSION} ${KERNEL_MODULES_DEB_DIR}/lib/modules/
-cp files/control ${KERNEL_MODULES_DEB_DIR}/DEBIAN
 
-fakeroot dpkg-deb --build ${KERNEL_MODULES_DEB_DIR}
-cp ${STAGE_WORK_DIR}/${PACKAGE_NAME}.deb ${ROOTFS_DIR}/var/cache/apt/archives
-
-on_chroot << EOF
-dpkg -i /var/cache/apt/archives/${PACKAGE_NAME}.deb
-EOF
+build_deb_package ${KERNEL_MODULES_DEB_DIR}
+install_deb_package ${PACKAGE_NAME}.deb
