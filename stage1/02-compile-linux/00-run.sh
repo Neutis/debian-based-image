24056414f6f11c9f62f9702444acd3d41587b819
yashin-alexander <alexandr.yashin@emlid.com>
2018-11-20 14:40:52 +0300

    stage1/02-compile-linux/00-run.sh: add linux-headers creation

--- stage1/02-compile-linux/00-run.sh
+++ stage1/02-compile-linux/00-run.sh
@@ -10,5 +10,6 @@ apply_patches $META_EMLID_NEUTIS_SRC/meta-neutis-bsp/recipes-kernel/linux/linux-
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- neutis_defconfig
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image dtbs modules -j $CPU_CORES
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- modules_install INSTALL_MOD_PATH=./build-modules
+make ARCH=arm64 CROSS_COMPILE=aarch-linux-gnu- headers_install INSTALL_HDR_PATH=./build-headers
 rm build-modules/lib/modules/$LINUX_VERSION/build build-modules/lib/modules/$LINUX_VERSION/source
 popd
