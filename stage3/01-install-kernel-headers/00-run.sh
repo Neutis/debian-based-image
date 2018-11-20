958df0c0ebb845112dc085afc5ec96f99629a29e
yashin-alexander <alexandr.yashin@emlid.com>
2018-11-20 14:41:52 +0300

    stage3: add substage for kernel-headers package installation

---
+++ stage3/01-install-kernel-headers/00-run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+PACKAGE_NAME=neutis-n5-kernel-headers
+KERNEL_HEADERS_DEB_DIR=${STAGE_WORK_DIR}/packages/${PACKAGE_NAME}
+
+mkdir -p ${KERNEL_HEADERS_DEB_DIR}/usr/src/linux-headers-${LINUX_VERSION}
+mkdir -p ${KERNEL_HEADERS_DEB_DIR}/DEBIAN
+rm -rf ${ROOTFS_DIR}/var/cache/apt/archives
+mkdir -p ${ROOTFS_DIR}/var/cache/apt/archives
+
+cp -R ${LINUX_SRC}/build-headers/include/* ${KERNEL_HEADERS_DEB_DIR}/usr/src/linux-headers-${LINUX_VERSION}
+cp files/control ${KERNEL_HEADERS_DEB_DIR}/DEBIAN
+
+build_deb_package ${KERNEL_HEADERS_DEB_DIR}
+install_deb_package ${PACKAGE_NAME}.deb
