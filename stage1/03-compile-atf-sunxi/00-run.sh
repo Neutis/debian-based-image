3b55d254881b93e019ba1c104acd434a47045621
yashin-alexander <alexandr.yashin@emlid.com>
2018-11-20 20:00:07 +0300

    stage1/03-compile-atf-sunxi/00-run.sh: add patches for atf-sunxi sources

--- stage1/03-compile-atf-sunxi/00-run.sh
+++ stage1/03-compile-atf-sunxi/00-run.sh
@@ -3,5 +3,6 @@
 export PATH="${GCC_LINARO_SRC}/bin:$PATH"
 
 pushd $ATF_SUNXI_SRC
+apply_patches ${META_SUNXI_SRC}/recipes-bsp/atf/files
 make CROSS_COMPILE=aarch64-linux-gnu- PLAT=sun50i_a64 DEBUG=1 bl31
 popd
