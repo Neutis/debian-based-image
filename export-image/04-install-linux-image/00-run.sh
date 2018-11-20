26a445f0dfe05c07ac08d47243028fb41c6e3a26
yashin-alexander <alexandr.yashin@emlid.com>
2018-11-20 20:51:50 +0300

    export-image/04-install-linux-image/00-run.sh: use separate functions for .deb install

--- export-image/04-install-linux-image/00-run.sh
+++ export-image/04-install-linux-image/00-run.sh
@@ -1,17 +1,12 @@
 #!/bin/bash -e
 
 PACKAGE_NAME=neutis-n5-linux-image
-LINUX_IMAGE_DEB_DIR=${STAGE_WORK_DIR}/${PACKAGE_NAME}
+LINUX_IMAGE_DEB_DIR=${STAGE_WORK_DIR}/packages/${PACKAGE_NAME}
 
 mkdir -p ${LINUX_IMAGE_DEB_DIR}/DEBIAN
 mkdir -p ${LINUX_IMAGE_DEB_DIR}/boot
 
 cp ${LINUX_SRC}/arch/arm64/boot/Image ${LINUX_IMAGE_DEB_DIR}/boot
 
-cp files/control ${LINUX_IMAGE_DEB_DIR}/DEBIAN
-fakeroot dpkg-deb --build ${LINUX_IMAGE_DEB_DIR}
-cp ${STAGE_WORK_DIR}/${PACKAGE_NAME}.deb ${ROOTFS_DIR}/var/cache/apt/archives
-
-on_chroot << EOF
-dpkg -i /var/cache/apt/archives/${PACKAGE_NAME}.deb
-EOF
+build_deb_package ${LINUX_IMAGE_DEB_DIR}
+install_deb_package ${PACKAGE_NAME}.deb
