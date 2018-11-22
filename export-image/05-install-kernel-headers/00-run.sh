477f8686724a82eea2171eb3da8ae4dc87c204b8
yashin-alexander <alexandr.yashin@emlid.com>
2018-11-22 15:48:14 +0300

    export-image/05-install-kernel-headers/00-run.sh: remove redundant directory clean-up

--- export-image/05-install-kernel-headers/00-run.sh
+++ export-image/05-install-kernel-headers/00-run.sh
@@ -5,8 +5,6 @@ KERNEL_HEADERS_DEB_DIR=${STAGE_WORK_DIR}/packages/${PACKAGE_NAME}
 
 mkdir -p ${KERNEL_HEADERS_DEB_DIR}/usr/src/linux-headers-${LINUX_VERSION}
 mkdir -p ${KERNEL_HEADERS_DEB_DIR}/DEBIAN
-rm -rf ${ROOTFS_DIR}/var/cache/apt/archives
-mkdir -p ${ROOTFS_DIR}/var/cache/apt/archives
 
 cp -R ${LINUX_SRC}/build-headers/include/* ${KERNEL_HEADERS_DEB_DIR}/usr/src/linux-headers-${LINUX_VERSION}
 cp files/control ${KERNEL_HEADERS_DEB_DIR}/DEBIAN
