46e59f07224f47381919c50995325927f6eb06b9
yashin-alexander <alexandr.yashin@emlid.com>
2018-11-23 19:41:12 +0300

    export-image/99-finalise/00-run.sh: move image to `deploy` after every build
    - add image unmount
    - remove `losetup -d` call

--- export-image/99-finalise/00-run.sh
+++ export-image/99-finalise/00-run.sh
@@ -2,4 +2,7 @@
 
 IMG_FILE="${STAGE_WORK_DIR}/${IMG_DATE}-${IMG_NAME}${IMG_SUFFIX}.img"
 rm -rf ${ROOTFS_DIR}/var/cache/apt/archives
-losetup -a | grep ${IMG_FILE} | cut -f 1 -d ' ' | sed 's/.$//' | xargs losetup -d
+unmount_image ${IMG_FILE}
+
+mkdir -p ${DEPLOY_DIR}
+mv ${IMG_FILE} ${DEPLOY_DIR}
