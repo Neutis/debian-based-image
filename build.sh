04dea7c93df238ae0c7df769098366609b957a71
yashin-alexander <alexandr.yashin@emlid.com>
2018-11-27 17:23:53 +0300

    build.sh: make separate functions for startup checks
    - place source of the lib-files before functions execution

--- build.sh
+++ build.sh
@@ -1,33 +1,19 @@
 #!/bin/bash -e
 
-if [[ $(systemd-detect-virt) == "none" ]]; then
-    echo "Execution of build.sh directly is deprecated.
-It may break your system.
-Please, run ./build-docker instead." 1>&2
-    exit 1
-fi
+export BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export LIB_DIR="${BASE_DIR}/lib"
 
-if [ "$(id -u)" != "0" ]; then
-	echo "Please run as root" 1>&2
-	exit 1
-fi
+source ${LIB_DIR}/stages.sh
+source ${LIB_DIR}/common.sh
+source ${LIB_DIR}/config.sh
 
-if [ -f config ]; then
-	source config
-fi
+check_build_environment
+check_root_rights
+apply_config_file
 
-if [ -z "${IMG_NAME}" ]; then
-	echo "IMG_NAME not set" 1>&2
-	exit 1
-fi
-if [ -z "${CPU_CORES}" ]; then
-	CPU_CORES=1
-fi
 
 export IMG_DATE=${IMG_DATE:-"$(date +%Y-%m-%d-%H:%M)"}
 
-export BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export LIB_DIR="${BASE_DIR}/lib"
 export WORK_DIR=${WORK_DIR:-"${BASE_DIR}/work/${IMG_NAME}"}
 export DEPLOY_DIR=${DEPLOY_DIR:-"${BASE_DIR}/deploy"}
 export LOG_FILE="${WORK_DIR}/build.log"
@@ -38,20 +24,15 @@ export SOURCES_DIR="${WORK_DIR}/sources"
 
 export LINUX_VERSION="4.17.2"
 export LINUX_SRC="${SOURCES_DIR}/linux"
-export LINUX_ARCHIVE_NAME="linux-${LINUX_VERSION}.tar.gz"
 
 export U_BOOT_VERSION="2018.07"
 export U_BOOT_SRC="${SOURCES_DIR}/u-boot"
-export U_BOOT_ARCHIVE_NAME="u-boot-${U_BOOT_VERSION}.tar.gz"
 
 export GCC_LINARO_VERSION="7.3.1"
 export GCC_LINARO_SRC="${SOURCES_DIR}/gcc-linaro"
-export GCC_LINARO_ARCHIVE_NAME="gcc-linaro.tar.gz"
 
 export ATF_SUNXI_SRC="${SOURCES_DIR}/atf-sunxi"
-
 export META_EMLID_NEUTIS_SRC="${SOURCES_DIR}/meta-emlid-neutis"
-
 export META_SUNXI_SRC="${SOURCES_DIR}/meta-sunxi"
 
 export CLEAN
@@ -74,10 +55,6 @@ export QUILT_PATCHES
 export QUILT_NO_DIFF_INDEX=1
 export QUILT_NO_DIFF_TIMESTAMPS=1
 export QUILT_REFRESH_ARGS="-p ab"
-
-source ${LIB_DIR}/stages.sh
-source ${LIB_DIR}/common.sh
-source ${LIB_DIR}/config.sh
 
 
 mkdir -p ${WORK_DIR}
