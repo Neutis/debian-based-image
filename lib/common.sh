e907ff1e2ee3a1d5fde71f73620ec30f3e45c438
yashin-alexander <alexandr.yashin@emlid.com>
2018-11-27 17:39:32 +0300

    lib/common.sh: fix stopping the build execution
    - Fix fall down with `rejected patches` while running e.g. second build
    - add the creation of the `PATCHED` file to run the set of builds

--- lib/common.sh
+++ lib/common.sh
@@ -113,10 +113,15 @@ apply_patches()
 {
   local patches_dir=$1
   local patchfiles=`ls $patches_dir | grep .patch`
-  for patch in $patchfiles; do
-    echo "Applying $patch patchfile."
-    patch --batch --silent -p1 -N < "${patches_dir}/${patch}"
-  done;
+  if [ -e "PATCHED" ]; then
+    echo "${patches_dir} is patched already. Nothing to do."
+  else
+    for patch in $patchfiles; do
+      echo "Applying $patch patchfile."
+      patch --batch --silent -p1 -N < "${patches_dir}/${patch}"
+    done;
+    touch "PATCHED"
+  fi
 }
 export -f apply_patches
 
