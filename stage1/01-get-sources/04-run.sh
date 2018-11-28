f7f0c560115cef4721b2fd6b1e23fc2d6421681a
yashin-alexander <alexandr.yashin@emlid.com>
2018-11-28 14:52:29 +0300

    stage1/01-get-sources: add curly braces to exclude names collisions for constants

--- stage1/01-get-sources/04-run.sh
+++ stage1/01-get-sources/04-run.sh
@@ -5,6 +5,6 @@ META_SUNXI_BRANCH="sumo"
 META_SUNXI_COMMIT_HASH=2c71a7d593f7d35b1bb8fec7aa3944b0295e036e
 
 if [ ! -d ${META_SUNXI_SRC} ]; then
-    git clone -b $META_SUNXI_BRANCH --single-branch $META_SUNXI_SOURCE_LINK $META_SUNXI_SRC
-    git -C $META_SUNXI_SRC checkout $META_SUNXI_COMMIT_HASH >-
+    git clone -b ${META_SUNXI_BRANCH} --single-branch ${META_SUNXI_SOURCE_LINK} ${META_SUNXI_SRC}
+    git -C ${META_SUNXI_SRC} checkout ${META_SUNXI_COMMIT_HASH} >-
 fi
--- stage1/01-get-sources/05-run.sh
+++ stage1/01-get-sources/05-run.sh
@@ -5,6 +5,6 @@ ATF_SOURCE_BRANCH="master"
 ATF_SOURCE_COMMIT_HASH=7db0c96023281d8a530f5e011a232e5d56557437
 
 if [ ! -d ${ATF_SUNXI_SRC} ]; then
-    git clone -b $ATF_SOURCE_BRANCH --single-branch $ATF_SOURCE_LINK $ATF_SUNXI_SRC
-    git -C $ATF_SUNXI_SRC checkout $ATF_SOURCE_COMMIT_HASH >-
+    git clone -b ${ATF_SOURCE_BRANCH} --single-branch ${ATF_SOURCE_LINK} ${ATF_SUNXI_SRC}
+    git -C ${ATF_SUNXI_SRC} checkout ${ATF_SOURCE_COMMIT_HASH} >-
 fi
