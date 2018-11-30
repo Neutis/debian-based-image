b204924653f07f4fd2da6a45d80ecb7f82545313
yashin-alexander <alexandr.yashin@emlid.com>
2018-11-30 15:38:32 +0300

    export-image/05-install-kernel-headers/00-run.sh: fix linux-headers path

--- export-image/05-install-kernel-headers/00-run.sh
+++ export-image/05-install-kernel-headers/00-run.sh
@@ -6,7 +6,7 @@ export PACKAGE_DEB_DIR=${STAGE_WORK_DIR}/packages/${PACKAGE_NAME}
 mkdir -p ${PACKAGE_DEB_DIR}/usr/src/linux-headers-${LINUX_VERSION}
 mkdir -p ${PACKAGE_DEB_DIR}/DEBIAN
 
-cp -R ${LINUX_SRC}/build-headers/include/* ${PACKAGE_DEB_DIR}/usr/src/linux-headers-${LINUX_VERSION}
+cp -R ${LINUX_SRC}/build-headers/* ${PACKAGE_DEB_DIR}/usr/src/linux-headers-${LINUX_VERSION}/
 cp files/control ${PACKAGE_DEB_DIR}/DEBIAN
 
 build_deb_package
